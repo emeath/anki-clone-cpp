e9ce476b62912f89552911298ac965440c5aab37
emeath <matheus.martins.jeronimo@gmail.com>
2022-09-22 22:45:17 -0300

    adds score system

--- anki-clone-cpp/main.cpp
+++ anki-clone-cpp/main.cpp
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-#include "Field.hpp"
-#include "Front.hpp"
-#include "Back.hpp"
 #include "Card.hpp"
 #include "Deck.hpp"
 
@@ -10,24 +7,39 @@ using namespace std;
 
 int main(int argc, char **argv)
 {	
-	AnkiClone::Card card1("What is the sky's color?", "Blue");
-	AnkiClone::Card card2("What is the earth's radius?", "6371 km");
-	AnkiClone::Card card3("What is the fifth planet on solar system?", "Jupiter");
-	AnkiClone::Card card4("How do I say goodbye in deutsch?", "Auf Wiedersehen");
+	vector<AnkiClone::Card> cards = {
+		AnkiClone::Card("What is the sky's color?", "Blue"),
+		AnkiClone::Card("What is the earth's radius?", "6371 km"),
+		AnkiClone::Card("What is the fifth planet on solar system?", "Jupiter"),
+		AnkiClone::Card("How do I say goodbye in deutsch?", "Auf Wiedersehen"),
+		};
 	
 	AnkiClone::Deck deck = string("General topic");
 	
-	deck.addCard(card1);
-	deck.addCard(card2);
-	deck.addCard(card3);
-	deck.addCard(card4);
+	for(AnkiClone::Card card : cards) {
+		deck.addCard(card);
+	}
+	
+	int score = 0;
 	
 	for(AnkiClone::Card card : deck.getCards()) {
+		string studentGuess;
 		cout << "Front: " << card.getFront().getField() << endl;
-		getchar();
+		cout << "Your answer: ";
+		cin >> studentGuess;
 		cout << "Back: " << card.getBack().getField() << endl;
+		
+		if(studentGuess.compare(card.getBack().getField()) == 0) {
+			score++;
+			cout << "Correct!" << endl;
+		} else {
+			cout << "Incorrect." << endl;
+		}
+		
 		cout << "===================================" << endl;
 	}
+	
+	cout << "You have answered correctly " << score << " and incorrectly " << deck.getCards().size() - score << " cards." << endl; 
 	
 	return 0;
 }
